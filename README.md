6418ad0f530752640f9c587aa590e8c1253b4285
Atsushi Nakatsugawa <atsushi@moongift.jp>
2017-10-11 23:10:37 +0900

    Update

---
+++ README.md
@@ -0,0 +1,10 @@
+# SquareのWebhook APIをRubyで利用するためのデモコード
+
+SquareのWebhook APIを使って決済が完了した際に受け取れるデモです。
+
+Herokuで使えますので手軽です。
+
+## License
+
+MIT License
+
--- app.rb
+++ app.rb
@@ -1,11 +1,3 @@
-# Demonstrates a Sinatra server listening for webhook notifications from the Square Connect API
-#
-# See Webhooks Overview for more information:
-# https://docs.connect.squareup.com/api/connect/v1/#webhooks-overview
-#
-# This sample requires the following gems:
-#   sinatra (http://www.sinatrarb.com/)
-#   unirest (http://unirest.io/ruby.html)
 $stdout.sync = true
 
 require 'base64'
@@ -15,23 +7,24 @@ require 'openssl'
 require 'sinatra'
 require 'square_connect'
 
-# Your application's access token
-ACCESS_TOKEN = 'sq0atp-hCgUIoG-UUWDOPvIftrrWQ'
+# アクセストークンを指定します
+ACCESS_TOKEN = 'YOUR_ACCESS_TOKEN'
 
-# Your application's webhook signature key, available from your application dashboard
-WEBHOOK_SIGNATURE_KEY = '_ABw6Ih64Z7bLxyddKHOcw'
+# Webhookの署名を指定します
+WEBHOOK_SIGNATURE_KEY = 'YOUR_WEBHOOK_SIGNATURE_KEY'
 
-# The URL that this server is listening on (e.g., 'http://example.com/events')
-# Note that to receive notifications from Square, this cannot be a localhost URL
+# WebhookのURLを指定します
 WEBHOOK_URL = 'https://fierce-mountain-94709.herokuapp.com/events'
 
+# Square connectの初期設定
 SquareConnect.configure do |config|
-  # Configure OAuth2 access token for authorization: oauth2
   config.access_token = ACCESS_TOKEN
 end
 
+# POST /events に対してWebhookがきます
 post '/events' do
   
+  # リクエストの妥当性をチェックします
   callback_body = request.body.string
   callback_signature = request.env['HTTP_X_SQUARE_SIGNATURE']
 
@@ -39,26 +32,31 @@ post '/events' do
     puts 'Webhook event with invalid signature detected!'
     return
   end
-
+  
+  # JSONデータの取得
   callback_body_json = JSON.parse(callback_body)
 
+  # PAYMENT_UPDATEイベントかどうかを判断します
   if callback_body_json.has_key?('event_type') and callback_body_json['event_type'] == 'PAYMENT_UPDATED'
-
+    
+    # テンプレートと取引IDを取得します
     location_id = callback_body_json['location_id']
     payment_id = callback_body_json['entity_id']
     
+    # Square APIにアクセスして決済情報の詳細を取得します
     api_instance = SquareConnect::V1TransactionsApi.new
     response = api_instance.retrieve_payment(location_id, payment_id)
     
     puts response
   end
   
+  # こちらはテストの場合です
   if callback_body_json.has_key?('event_type') and callback_body_json['event_type'] == 'TEST_NOTIFICATION'
     puts callback_body_json
   end
 end
 
-
+# リクエストの妥当性チェックを行います
 def is_valid_callback(callback_body, callback_signature)
   string_to_sign = WEBHOOK_URL + callback_body
   string_signature = Base64.strict_encode64(OpenSSL::HMAC.digest('sha1', WEBHOOK_SIGNATURE_KEY, string_to_sign))
